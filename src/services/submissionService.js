9d1595ad34620f60da9a89ad85daaf844495546d
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2026-02-06 14:52:13 +0530

    commit

--- src/services/submissionService.js
+++ src/services/submissionService.js
@@ -1,21 +1,26 @@
 import submissionQueueProducer from "../producers/submissionQueueProducer.js";
-
+import InternalServerError from "../errors/internalServerError.js";
 class SubmissionService {
     constructor (submissionRepository) {
         //inject here
         this.submissionRepository = submissionRepository;
     }
-
+    
     async addSubmission(submissionPayload) {
-        const submission = await this.submissionRepository.createSubmission(submissionPayload);
-        //TODO: Add error system.
-        if(!submission) {
-            throw {message: "Not able to create submission"};
+        try {
+            const submission = await this.submissionRepository.createSubmission(submissionPayload);
+        
+            if(!submission) {
+                throw new InternalServerError("unable to create submission");
+            }
+            console.log("service",submission);
+            const response = await submissionQueueProducer(submission);
+            console.log("queue", response);
+            return {submission};
+        } catch (error) {
+            console.log(error);
+            return {error: error};
         }
-        console.log("service",submission);
-        const response = await submissionQueueProducer(submission);
-        console.log("queue", response);
-        return {submission};
     }  
 }
 export default SubmissionService;
